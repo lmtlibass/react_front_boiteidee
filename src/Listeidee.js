2422580d84999f8fae066dc47af2a2663672ec3c
Honorable con <honorablecon@gmail.com>
2022-02-25 17:31:47 +0000

    add suggestion card

--- src/Listeidee.js
+++ src/Listeidee.js
@@ -1,71 +1,28 @@
-
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import SuggestionCard from './components/Card.js';
+import {Stack } from 'react-bootstrap';
+
 
 
 function Listeidee() {
 
-    const [loading, setLoading] = useState(true);
-    const [idee, setIdeas] = useState([]);
+    const [suggestions, setSuggestions] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-    useEffect(() => {
-
-        axios.get(`http://127.0.0.1:8000/api/listeidee`).then(res=>{
-            
-                setIdeas(res.data.idee)
-                setLoading(false);
-            
-        });
-
-    }, []);
-
-
-    if(loading)
-    {
-        return <h4 className='mt-4'>Chargement des idées...</h4>
-    }
-    else
-    {
-        var idee_HTMLTABLE = "";
-       
-        idee_HTMLTABLE = idee.map( (item, index) => {
-            return (
-                <tr key={index}>
-                    <td>{item.titre}</td>
-                    <td>{item.description}</td>
-                    <td>{item.statut}</td>
-                </tr>
-            );
-        });
-    }
-console.warn(idee);
+    useEffect( ()=> {
+        fetch('https://exemple-api.herokuapp.com/api/apprenants')
+            .then( res => res.json())
+            .then( data => {
+                setSuggestions(data);
+                setIsLoaded(true);
+            })
+    });
     return (
-        <div>
-            <div className="container mt-4">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="card">
-                            <div className="card-body">
-             
-                                <table className="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Titre</th>
-                                            <th>Description</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {idee_HTMLTABLE}
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Stack direction="horizontal" gap={3} className="p-5">{
+            isLoaded && suggestions.map(suggestion => 
+                    <SuggestionCard suggestion={suggestion}/>
+            )
+        }</Stack>
     );
 
 }
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import { Card } from 'react-bootstrap';
+
+export default function SuggestionCard({suggestion}){
+
+    return (
+        <div>
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>{suggestion.prenom} {suggestion.nom}</Card.Title>
+                <Card.Text>{suggestion.email}</Card.Text>
+                <Card.Link href="#">approuvée</Card.Link>
+                <Card.Link href="#">refusée</Card.Link>
+            </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+{/* <Card style={{ width: '18rem' }}>
+<Card.Body>
+    <Card.Title>{props.dool}</Card.Title>
+    <Card.Text>{props.dool}</Card.Text>
+    <Card.Link href="#">approuvée</Card.Link>
+    <Card.Link href="#">refusée</Card.Link>
+</Card.Body>
+</Card> */}
